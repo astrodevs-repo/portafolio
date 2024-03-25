d233175b4f22f3d04c69c8145546f1de30c6d378
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-03-24 21:12:43 -0300

    refactor

--- src/Components/Home/Section1.jsx
+++ src/Components/Home/Section1.jsx
@@ -1,41 +1,10 @@
 import "./SectionScroll.scss";
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
 import Carousel from "../shared/ScrollText";
 import Section from "../shared/Section/Section";
 import Container from "../Container/Container";
 
 const SectionScroll = ({ data }) => {
-  const [activeSection, setActiveSection] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const sections = data.data.map((_, index) => {
-        return document.getElementById(`section${index + 1}`).getBoundingClientRect();
-      });
-
-      const centerHeight = window.innerHeight / 2;
-
-      const currentSectionIndex = sections.findIndex((section) => {
-        return section.top <= centerHeight && section.bottom >= centerHeight;
-      });
-
-      setActiveSection(currentSectionIndex >= 0 ? currentSectionIndex + 1 : 0);
-    };
-
-    const handleInitialLoad = () => {
-      setActiveSection(0); // Set activeSection to 0 when the component mounts
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("load", handleInitialLoad);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("load", handleInitialLoad);
-      setActiveSection(0);
-    };
-  }, [data.data]);
-
   return (
     <Section id={data.href}>
       <div style={{ backgroundColor: data.Bg, textColor: data.textColor }} className="h-full">
@@ -55,15 +24,10 @@ const SectionScroll = ({ data }) => {
 SectionScroll.propTypes = {
   data: PropTypes.shape({
     header: PropTypes.string.isRequired,
-    data: PropTypes.arrayOf(
-      PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        href: PropTypes.string.isRequired,
-        textColor: PropTypes.string.isRequired,
-        Bg: PropTypes.string.isRequired,
-      })
-    ).isRequired,
+    data: PropTypes.string.isRequired,
+    href: PropTypes.string.isRequired,
+    Bg: PropTypes.string.isRequired,
+    textColor: PropTypes.string.isRequired,
   }).isRequired,
 };
 
