3fcb1d361e09c7c798bebc01b8301d4543763722
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-09-01 17:03:13 -0300

    refactor

--- src/Components/shared/Smoked/Smoked.jsx
+++ src/Components/shared/Smoked/Smoked.jsx
@@ -1,12 +1,28 @@
 // Background.tsx
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import png from "/public/atomgrueso.png"; // Asegúrate de que esta ruta sea correcta
 
 const Background = () => {
+  // Estado para la posición de los elementos
+  const [scrollY, setScrollY] = useState(0);
+
+  // Actualiza el estado del scroll
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  // Añade y limpia el listener de scroll
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   // Crea una función para generar una posición, tamaño y rotación aleatorios
   const getRandomTransform = () => {
-    const size = Math.random() * 150 + 350; // Tamaño entre 50 y 250 px
+    const size = Math.random() * 150 + 350; // Tamaño entre 350 y 500 px
     const rotation = Math.random() * 360; // Rotación entre 0 y 360 grados
     const x = Math.random() * 100; // Posición X en porcentaje
     const y = Math.random() * 100; // Posición Y en porcentaje
@@ -20,7 +36,7 @@ const Background = () => {
   };
 
   // Crea una lista de elementos de imagen con transformaciones aleatorias
-  const elements = Array.from({ length: 30 }).map((_, i) => (
+  const elements = Array.from({ length: 10 }).map((_, i) => (
     <motion.div
       key={i}
       className="absolute"
@@ -31,19 +47,18 @@ const Background = () => {
         backgroundRepeat: "no-repeat",
       }}
       animate={{
-        rotate: [0, 360],
-        y: 40,
+        y: -scrollY, // Mueve el elemento hacia arriba según el scroll
       }}
       transition={{
-        repeat: Infinity,
-        duration: 100, // Cambia la duración según la velocidad que desees
+        type: "tween",
+        duration: 0.5, // Cambia la duración para ajustar la velocidad del movimiento
         ease: "linear",
       }}
     />
   ));
 
   return (
-    <div className="absolute top-0 left-0 z-0 w-full h-screen overflow-hidden opacity-20">
+    <div className="absolute top-0 left-0 z-0 w-full h-screen overflow-hidden opacity-30">
       {elements}
     </div>
   );
