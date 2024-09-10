6192344ef4581f3eba91b4c05a30f8c10c801be3
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-09-10 18:34:37 -0300

    refactor

--- src/Components/Home/Counter/Counter.jsx
+++ src/Components/Home/Counter/Counter.jsx
@@ -4,24 +4,25 @@ import CountUp from "../../shared/CountUp/CountUp";
 const Counter = ({ index, onObserver, currentStep }) => {
   const data = [
     {
-      label: "Equipo especializado",
-      value: 200,
+      label: "Proyectos",
+      value: 15,
       description:
-        "Contamos con un equipo especializado para poder alcanzar los objetivos con múltiples habilidades. Cada proyecto es un nuevo desafío que resolvemos de manera creativa y funcional.",
+        "Contamos con un equipo especializado para alcanzar los objetivos con múltiples habilidades. Cada proyecto es un nuevo desafío que resolvemos de manera creativa y funcional.",
     },
     {
       label: "Países",
-      value: 9,
+      value: 5,
       description:
-        "Ser nómades es trabajar a nivel global, investigando y analizando cada coyuntura cultural para convertirnos en auténticos locales en la comunicación hacia la audiencia.",
+        "Trabajamos con clientes de diferentes países, lo que nos permite adaptar nuestras soluciones a una variedad de contextos y necesidades internacionales.",
     },
     {
-      label: "Clientes satisfechos",
-      value: 95,
+      label: "Clientes Satisfechos",
+      value: 8,
       description:
-        "Nuestros clientes confían en nosotros bajo la premisa de que cada uno es diferente, por lo que necesitan una estrategia personalizada desde la creatividad hasta la gestión.",
+        "Nuestros clientes confían en nosotros debido a nuestra capacidad para ofrecer estrategias personalizadas, desde la creatividad hasta la gestión, adaptadas a cada necesidad específica.",
     },
   ];
+
   return (
     <Section currentStep={currentStep} onObserver={onObserver} index={index}>
       <section className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-20">
