54ea1f7d36934fe9d8bf326477299460e1537ab2
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-10-03 11:00:04 -0300

    refactor

--- src/Components/shared/Section/Section.jsx
+++ src/Components/shared/Section/Section.jsx
@@ -2,24 +2,17 @@ import Container from "../../Container/Container";
 import Inspector from "../Inspector/Inspector";
 import SubTitleItalic from "../SubTitleItalic";
 import Title from "../Title";
-import { motion } from "framer-motion";
 
-const Section = ({ id, children, bg, title, index, currentStep, extra, subtitle, onObserver }) => {
+const Section = ({ id, children, bg, title, index, extra, subtitle, onObserver }) => {
   return (
     <Inspector index={index} onObserver={onObserver}>
-      <motion.section
-        className={`w-full h-fit-content ${bg} py-5 sm:py-20 ${extra}`}
-        id={id}
-        initial={{ opacity: 0 }} // Start with opacity 0
-        animate={index === currentStep ? { opacity: 1 } : { opacity: 0 }} // Animate opacity based on currentStep
-        transition={{ duration: 1, ease: "easeInOut" }} // Adjust the transition properties as needed
-      >
+      <section className={`w-full h-fit-content ${bg} py-5 sm:py-20 ${extra}`} id={id}>
         <Container>
           <Title text={title} textColor={"text-blackCeniza"} extra={"font-LusitanaBold"} />
           {subtitle ? <SubTitleItalic text={subtitle} textColor={"text-blackCeniza"} /> : ""}
           {children}
         </Container>
-      </motion.section>
+      </section>
     </Inspector>
   );
 };
