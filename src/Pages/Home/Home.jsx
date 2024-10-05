e61d2425fdebc56fd3c13691600bac40cb16c7eb
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-10-05 11:24:12 -0300

    refactor

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,16 @@
-import { useState, Suspense, lazy } from "react";
-import Loading from "../../Components/shared/Loading/Loading";
-
-const About = lazy(() => import("../../Components/About/About"));
-const Header = lazy(() => import("../../Components/Home/Header/Header"));
-const Team = lazy(() => import("../../Components/Home/team/Team"));
-const Footer = lazy(() => import("../../Components/Footer/Footer"));
-const StepList = lazy(() => import("../../Components/Home/Stepper/Stepper"));
-const ClientsSection = lazy(() => import("../../Components/Home/SkillsSection/SkillsSection"));
-const Servicios = lazy(() => import("../../Components/Home/Servicios/Servicios"));
-const Blog = lazy(() => import("../../Components/Home/Blog/Blog"));
-const Navbar = lazy(() => import("../../Components/Navbar/Navbar"));
-const SmokeBackground = lazy(() => import("../../Components/shared/Smoked/Smoked"));
-const ProgressBar = lazy(() => import("../../Components/shared/ProgressBar/ProgressBar"));
-const Counter = lazy(() => import("../../Components/Home/Counter/Counter"));
+import { useState } from "react";
+import About from "../../Components/About/About";
+import Header from "../../Components/Home/Header/Header";
+import Team from "../../Components/Home/team/Team";
+import Footer from "../../Components/Footer/Footer";
+import StepList from "../../Components/Home/Stepper/Stepper";
+import ClientsSection from "../../Components/Home/SkillsSection/SkillsSection";
+import Servicios from "../../Components/Home/Servicios/Servicios";
+import Blog from "../../Components/Home/Blog/Blog";
+import Navbar from "../../Components/Navbar/Navbar";
+import SmokeBackground from "../../Components/shared/Smoked/Smoked";
+import ProgressBar from "../../Components/shared/ProgressBar/ProgressBar";
+import Counter from "../../Components/Home/Counter/Counter";
 
 const Home = () => {
   const [currentStep, setCurrentStep] = useState("1");
@@ -20,30 +18,22 @@ const Home = () => {
 
   return (
     <>
-      <Suspense
-        fallback={
-          <section className="w-screen h-screen flex justify-center items-center">
-            <Loading />
-          </section>
-        }
-      >
-        <Navbar />
-        <ProgressBar />
-        <SmokeBackground />
-        <Header index={"home"} onObserver={handleStepChange} currentStep={currentStep} />
-        <About index={"aboutus"} onObserver={handleStepChange} currentStep={currentStep} />
-        <Counter index={"counter"} onObserver={handleStepChange} currentStep={currentStep} />
-        <Servicios index={"quienessomos"} onObserver={handleStepChange} currentStep={currentStep} />
-        <ClientsSection index={"skills"} onObserver={handleStepChange} currentStep={currentStep} />
-        <StepList
-          index={"comotrabajamos"}
-          onObserver={handleStepChange}
-          currentStepAnother={currentStep}
-        />
-        <Blog index={"blog"} onObserver={handleStepChange} currentStep={currentStep} />
-        <Team index={"team"} onObserver={handleStepChange} currentStep={currentStep} />
-        <Footer index={"contactanos"} onObserver={handleStepChange} currentStep={currentStep} />
-      </Suspense>
+      <Navbar />
+      <ProgressBar />
+      <SmokeBackground />
+      <Header index={"home"} onObserver={handleStepChange} currentStep={currentStep} />
+      <About index={"aboutus"} onObserver={handleStepChange} currentStep={currentStep} />
+      <Counter index={"counter"} onObserver={handleStepChange} currentStep={currentStep} />
+      <Servicios index={"quienessomos"} onObserver={handleStepChange} currentStep={currentStep} />
+      <ClientsSection index={"skills"} onObserver={handleStepChange} currentStep={currentStep} />
+      <StepList
+        index={"comotrabajamos"}
+        onObserver={handleStepChange}
+        currentStepAnother={currentStep}
+      />
+      <Blog index={"blog"} onObserver={handleStepChange} currentStep={currentStep} />
+      <Team index={"team"} onObserver={handleStepChange} currentStep={currentStep} />
+      <Footer index={"contactanos"} onObserver={handleStepChange} currentStep={currentStep} />
     </>
   );
 };
