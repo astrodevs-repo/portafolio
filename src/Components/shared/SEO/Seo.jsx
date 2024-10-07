4f4fcca5e94cfe83faa871e38842a56673a999c6
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-10-07 20:32:07 -0300

    refactor

--- src/Components/shared/SEO/Seo.jsx
+++ src/Components/shared/SEO/Seo.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-
 import { Helmet } from "react-helmet-async";
 
-const SEO = React.memo(function SEO({ title, description, name, type }) {
+const SEO = React.memo(function SEO({ title, description, name, type, url, image }) {
   return (
     <Helmet>
-      {/* Standard metadata tags */}
       <title>{title}</title>
       <meta name="description" content={description} />
-      {/* End standard metadata tags */}
-      {/* Facebook tags */}
+
+      {/* Open Graph tags */}
       <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      {/* End Facebook tags */}
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+
       {/* Twitter tags */}
       <meta name="twitter:creator" content={name} />
       <meta name="twitter:card" content={type} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      {/* End Twitter tags */}
+      <meta name="twitter:image" content={image} />
     </Helmet>
   );
 });
+
 export default SEO;
