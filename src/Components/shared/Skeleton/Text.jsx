da062be4eeebff087b1e0b28161a9eb68e98faab
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-10-12 10:10:28 -0300

    refactor

--- src/Components/shared/Skeleton/Text.jsx
+++ src/Components/shared/Skeleton/Text.jsx
@@ -1,10 +1,10 @@
 // SkeletonButton.js
 import React from "react";
 
-const SkeletonText = React.memo(function SkeletonText({ width, height }) {
+const SkeletonText = React.memo(function SkeletonText({ width, height, extra }) {
   return (
     <section
-      className={`h-5 bg-BlueNeurons/60  dark:bg-gray-700 w-48 ${width} ${height}`}
+      className={`h-5 bg-BlueNeurons/60  dark:bg-gray-700 w-48 ${width} ${height}  ${extra}`}
     ></section>
   );
 });
