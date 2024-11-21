3890c3d46bbccb4f760ba957368c447c68f75a2f
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-11-21 18:12:45 -0300

    refactor

--- src/Components/Home/Header/Header.jsx
+++ src/Components/Home/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header({ index }) {
   return (
     <section
       id={index}
-      className="min-h-screen bg-gradient-to-bl from-BlueNeurons/50 via-[#e9eeffe2] to-[#e9eeff] dark:from-dark-BlueNeurons/20 dark:via-gray-800/90 dark:to-gray-800 sm:gap-10 flex justify-center sm:justify-center items-center relative px-5 sm:px-5 md:px-10 lg:px-10 xl:px-40 2xl:px-32 3xl:px-[200px] pt-28 sm:pt-0"
+      className="min-h-screen bg-gradient-to-bl from-BlueNeurons/50 via-purple-400/20  to-[#e9eeff] dark:from-dark-BlueNeurons/20 dark:via-gray-800/90 dark:to-gray-800 sm:gap-10 flex justify-center sm:justify-center items-center relative px-5 sm:px-5 md:px-10 lg:px-10 xl:px-40 2xl:px-32 3xl:px-[200px] pt-28 sm:pt-0"
     >
       <section className="grid grid-cols-1 sm:grid-cols-3 w-full gap-0 sm:gap-10 max-w-[1900px]">
         <section className="flex flex-col justify-center items-start gap-5 col-span-2">
