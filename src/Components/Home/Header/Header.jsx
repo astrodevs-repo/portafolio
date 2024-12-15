30d3902af4d20076d274451459614f2e1f77bfcf
Jesús Eduardo Moreno <70926975+librotero@users.noreply.github.com>
2024-12-15 19:20:20 -0300

    refactor

--- src/Components/Home/Header/Header.jsx
+++ src/Components/Home/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header({ index, title1, title2, title3, form }) {
   return (
     <section
       id={index}
-      className="min-h-screen bg-gradient-to-bl from-[#affffe]/50 via-BlueNeurons/50  to-[#6352ff]/50 dark:from-dark-BlueNeurons/20 dark:via-gray-800/90 dark:to-gray-800 sm:gap-10 flex justify-center sm:justify-center items-center relative px-5 sm:px-5 md:px-10 lg:px-10 xl:px-40 2xl:px-32 3xl:px-[200px] pt-28 sm:pt-0"
+      className="min-h-screen bg-gradient-to-bl  dark:from-dark-BlueNeurons/20 dark:via-gray-800/60 dark:to-gray-800  sm:gap-10 flex justify-center sm:justify-center items-center relative px-5 sm:px-5 md:px-10 lg:px-10 xl:px-40 2xl:px-32 3xl:px-[200px] pt-28 sm:pt-0"
     >
       <section className="grid grid-cols-1 sm:grid-cols-3 w-full gap-0 sm:gap-10 max-w-[1900px]">
         <section className="flex flex-col justify-center items-start gap-5 col-span-2 md:col-span-3 lg:col-span-2">
@@ -46,6 +46,8 @@ function Header({ index, title1, title2, title3, form }) {
           </Suspense>
         </section>
       </section>
+
+      <section className="header__background"></section>
     </section>
   );
 }
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,9 +3,30 @@ import "react-toastify/dist/ReactToastify.css";
 import { ReactLenis } from "lenis/dist/lenis-react";
 import Footer from "../Components/Footer/Footer";
 import { useLang } from "../context/useLang";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const Layout = ({ children }) => {
   const data = useLang();
+
+  const [isVisible, setIsVisible] = useState(true);
+  useEffect(() => {
+    // Escuchar el scroll para ocultar el botón
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        // Cambia 50 por el valor que necesites
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   if (!data) {
     return <div>Loading...</div>; // Display loading state until data is available
@@ -25,16 +46,50 @@ const Layout = ({ children }) => {
         href="https://wa.me/+5492612336104"
         target="_blank"
         rel="noopener noreferrer"
-        className="fixed bottom-10 right-0"
+        className="fixed bottom-0 right-0"
         style={{
           zIndex: "1000",
         }}
       >
-        <img
-          src="https://i.pinimg.com/originals/16/71/c0/1671c09a32f98796ede36fabfab4dee7.png"
-          alt=""
-          className="w-24 h-24 sm:w-32 sm:h-32"
-        />
+        <motion.section
+          className="relative"
+          initial={{ opacity: 0 }} // Empieza invisible
+          animate={{ opacity: 1 }} // Se anima a aparecer
+          transition={{ duration: 0.5 }}
+        >
+          <motion.img
+            src="https://i.pinimg.com/originals/16/71/c0/1671c09a32f98796ede36fabfab4dee7.png"
+            alt="Notification"
+            className="w-24 h-24 sm:w-32 sm:h-32"
+            initial={{ scale: 0 }} // Comienza pequeño
+            animate={{ scale: 1 }} // Se anima a su tamaño original
+            transition={{ duration: 0.5 }}
+          />
+          <span className="sr-only">Notifications</span>
+
+          <motion.div
+            className="absolute top-5 left-5 inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full dark:border-gray-900"
+            initial={{ scale: 0 }} // Comienza pequeño
+            animate={{ scale: 1 }} // Se anima a su tamaño original
+            transition={{
+              duration: 0.5,
+              delay: 5, // Aparece después de 5 segundos
+            }}
+          >
+            1
+          </motion.div>
+
+          {isVisible && (
+            <motion.div
+              className="absolute bottom-10 right-28 transform -translate-x-full p-2 bg-white text-black rounded-md min-w-72"
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <span className="block">Escribenos aca, somos Neurons</span>
+            </motion.div>
+          )}
+        </motion.section>
       </a>
       <ToastContainer
         position="top-right"
